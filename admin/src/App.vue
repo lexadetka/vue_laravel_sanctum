dcc38f8724c3f8df9b572957d234a5b22860fd86
lexadetka <bychkovski.007@gmail.com>
2023-06-29 09:15:44 +0300

    initial

---
+++ admin/src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import 'bootstrap/dist/css/bootstrap.css'
+
+</script>
+
+<template>
+<div class="d-flex flex-column w-25">
+  <button class="btn btn-primary" @click="login">Login</button>
+  <button class="btn btn-outline-success">testGet</button>
+</div>
+</template>
+<script>
+import axios from 'axios'
+import 'bootstrap/dist/js/bootstrap'
+export default {
+  name: 'App',
+  methods: {
+    login(){
+      axios.get('http://api.alexeybychkovski.site/sanctum/csrf-cookie')
+          .then(r=>{
+            axios.post('http://api.alexeybychkovski.site/login', {email: 'user@mail.ru', password: '123'})
+                .then(res=>{
+                  console.log(res)
+                })
+          })
+    },
+    testGet(){
+      axios.get('http://api.alexeybychkovski.site/api/user')
+          .then(res=>{
+            console.log(res)
+          })
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
---
+++ frontend/src/App.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import 'bootstrap/dist/css/bootstrap.css'
+axios.defaults.withCredentials = true;
+</script>
+
+<template>
+  <div>
+    123
+    <button @click="login" class="btn btn-outline-success">Login</button>
+    <button @click="testGet" class="btn btn-primary">test get</button>
+    <button @click="getUser" class="btn btn-primary">get User</button>
+  </div>
+</template>
+<script>
+import 'bootstrap/dist/js/bootstrap'
+import axios from 'axios'
+export default {
+  name: 'App',
+  mounted() {
+    // this.testGet()
+  },
+  methods: {
+    testGet(){
+      axios.get('http://api.alexeybychkovski.site/api/get')
+          .then(res=>{
+            console.log(res)
+          })
+    },
+    getUser(){
+      axios.get('http://api.alexeybychkovski.site/api/user')
+          .then(res=>{
+            console.log(res)
+          })
+    },
+    login(){
+      axios.get('http://api.alexeybychkovski.site/sanctum/csrf-cookie')
+          .then(r=>{
+            axios.post('http://api.alexeybychkovski.site/login',{email:'user@mail.ru', password: '123'})
+                .then(res=>{
+                  console.log(res)
+                })
+          })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
